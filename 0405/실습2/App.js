d46a7c8b1de5764392db77b778226fc4c5ee3181
geumbi4 <137870178+geumbi4@users.noreply.github.com>
2024-05-04 21:39:07 +0900

    Add files via upload

---
+++ 0405/실습2/App.js
@@ -0,0 +1,16 @@
+import { createElement } from 'react';
+function Greeting({ name }) {
+return createElement(
+'h1',
+{ className: 'greeting' },
+'Hello ',
+createElement('i', null, name),
+'. Welcome!'
+);
+}
+export default function App() {
+return createElement(
+Greeting,
+{ name: 'Taylor' }
+);
+}
