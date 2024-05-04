021082795ee850d68cb8b282210b72f95c39055a
geumbi4 <137870178+geumbi4@users.noreply.github.com>
2024-05-04 23:00:22 +0900

    Add files via upload

---
+++ 0412/실습4,5,6/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import Main from './Main';
+function App() {
+return (
+<div>
+<Header />
+<Main name="홍길동"/>
+<Footer />
+</div>
+);
+}
+export default App;
