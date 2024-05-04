5381d242a7aa03fd2d261f92afb324422e321de2
geumbi4 <137870178+geumbi4@users.noreply.github.com>
2024-05-04 23:44:24 +0900

    Add files via upload

---
+++ 0412/실습4,5,6/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FPhotoText from './FPhotoText';
+function App() {
+return (
+<div>
+<FPhotoText image="computer" label = "Desktop" />
+<FPhotoText image="laptop" label = "NoteBook" />
+<FPhotoText image="pad" label = "Pad" />
+</div>
+);
+}
+export default App;
