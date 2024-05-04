635a4e9f0980e06cd43a4ac642ba58c64497e3f1
geumbi4 <137870178+geumbi4@users.noreply.github.com>
2024-05-04 23:05:58 +0900

    Add files via upload

---
+++ 0412/실습4,5,6/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import Main from './Main';
+function App() {
+return (
+<div>
+<Header />
+<Main name={9} color="blue"/>
+<Footer />
+</div>
+);
+}
+export default App;
