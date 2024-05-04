612aa54c465df48465d430e58e3f1a9be57844c9
geumbi4 <137870178+geumbi4@users.noreply.github.com>
2024-05-04 23:57:15 +0900

    Add files via upload

---
+++ 0412/실습4,5,6/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CPhotoText from './CPhotoText';
+function App() {
+return (
+<div>
+<CPhotoText image="computer" label = "Desktop" />
+<CPhotoText image="laptop" label = "NoteBook" />
+<CPhotoText image="pad" label = "Pad" />
+</div>
+);
+}
+export default App;
