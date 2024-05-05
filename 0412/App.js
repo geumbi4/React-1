908f19506b72558256cd469bf316a06c92a28327
geumbi4 <137870178+geumbi4@users.noreply.github.com>
2024-05-05 22:17:53 +0900

    Add files via upload

---
+++ 0412/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import My from './My';
+import './App.css';
+
+function App() {
+
+return (
+  <div>
+    <My title="자기소개" content="대학생"/>
+  </div>
+);
+}
+
+export default App;
---
+++ 0412/My.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function My(props) {
+    return (
+      <div>
+        <h1>{props.title}</h1>
+        <p>{props.content}</p>
+    </div>
+    );
+  }
+export default My;
