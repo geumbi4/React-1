ed7509b5bf3eb3bcdf8df50ab61fb8a5c8e11a10
geumbi4 <137870178+geumbi4@users.noreply.github.com>
2024-05-05 22:45:36 +0900

    Add files via upload

---
+++ 0419/실습1/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+export default class App extends React.Component{
+constructor(props) {
+super(props);
+this.state = {
+name: '홍길동',
+age: 300,
+job: 'developer',
+};
+}
+render() {
+const {name, age, job} = this.state;
+return (
+<div style={{margin:15}}>
+<div>name: {name}</div>
+<div>age: {age}</div>
+<div>job: {job}</div>
+</div>
+);
+}
+}
