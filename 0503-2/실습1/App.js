4dcb9e5597677f0fe55bc4c23cde2100afc36402
geumbi4 <137870178+geumbi4@users.noreply.github.com>
2024-05-06 22:00:54 +0900

    Add files via upload

---
+++ 0503-2/실습1/App.js
@@ -0,0 +1,11 @@
+// App.js
+import React, { useState } from "react";
+import SmartHome from "./SmartHome";
+const App = () => {
+return (
+<div style={{ position: "absolute", top: "50%", left: "50%" }}>
+<SmartHome />
+</div>
+);
+};
+export default App;
