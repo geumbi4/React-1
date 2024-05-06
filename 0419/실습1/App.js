77a65838d3f012424ecbd689efbdb3b96f4616cc
geumbi4 <137870178+geumbi4@users.noreply.github.com>
2024-05-06 14:21:33 +0900

    Add files via upload

---
+++ 0419/실습1/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Stopwatch from './Stopwatch';
+import './App.css';
+
+function App() {
+
+return (
+  <div>
+    <Stopwatch/>   
+  </div>
+);
+}
+
+export default App;
