e0d14272407ffac5c809aed95e27e570e4864034
geumbi4 <137870178+geumbi4@users.noreply.github.com>
2024-05-06 14:41:06 +0900

    Add files via upload

---
+++ 0419/실습1/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NumberForm from './NumberForm';
+import './App.css';
+
+function App() {
+
+return (
+  <div>
+    <NumberForm/>   
+  </div>
+);
+}
+
+export default App;
