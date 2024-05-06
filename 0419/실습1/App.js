4714f98648733c0461413acdf43b0687aa4780bf
geumbi4 <137870178+geumbi4@users.noreply.github.com>
2024-05-06 14:51:08 +0900

    Add files via upload

---
+++ 0419/실습1/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MultiForm from './MultiForm';
+import './App.css';
+
+function App() {
+
+return (
+  <div>
+    <MultiForm/>   
+  </div>
+);
+}
+
+export default App;
