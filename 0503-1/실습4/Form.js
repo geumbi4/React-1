eb33153fc9dd2ec6bef7868f44535237437a14ab
geumbi4 <137870178+geumbi4@users.noreply.github.com>
2024-05-06 20:20:32 +0900

    Add files via upload

---
+++ 0503-1/실습4/Form.js
@@ -0,0 +1,30 @@
+import { forwardRef } from 'react';
+import React, { useRef } from "react"
+
+const MyInput = forwardRef(function MyInput(props, ref) {
+const { label, ...otherProps } = props;
+return (
+<label>
+{label}
+<input {...otherProps} ref={ref} />
+</label>
+);
+});
+
+
+function Form() {
+  const ref = useRef(null);
+  function handleClick() {
+  ref.current.focus();
+  }
+  return (
+  <form>
+  <MyInput label="Enter your name:" ref={ref} />
+  <button type="button" onClick={handleClick}>
+  Edit
+  </button>
+  </form>
+  );
+  }
+
+  export default Form;
